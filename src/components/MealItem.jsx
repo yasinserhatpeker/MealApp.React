aedeae6757c21b523d5bfe605ed670f9c03655a8
Yasin <yasinpeker43@gmail.com>
2025-05-07 21:58:06 +0300

    MealItem component structure is done.

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -1,3 +1,17 @@
-export default function MealItem() {
-    
+export default function MealItem({meal}) {
+    return (
+        <li id="meal-item">
+            <article>
+                <img src={meal.image} alt="meal image" />
+                <div>
+                    <h3>{meal.name}</h3>
+                    <p className="meal-item-price">{meal.price}</p>
+                    <p className="meal-item-description">{meal.description}</p>
+                </div>
+                <p className="meal-item-actions">
+                     <button>Add to cart</button>
+                    </p>
+            </article>
+        </li>
+    )
 }
