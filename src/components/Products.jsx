2456d561f2c76752df2a985a00ce6d63770d9992
Yasin <yasinpeker43@gmail.com>
2025-05-07 21:35:15 +0300

    fixed some typo.

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -5,22 +5,23 @@ const [loadedMeals,setLoadedMeals]=useState([]);
 
 useEffect(()=> {
     async function fetchingMeals() {
-       const  response =  await fetch('http:/localhost:3000/meals');
+       const response =  await fetch('http://localhost:3000/meals');
     
        if(!response.ok) {
           // will be added later
        }
-       const meals=await response.json();
+       const meals= await response.json();
        setLoadedMeals(meals);
     }
     fetchingMeals();
+    
 },[])
 
   return (
         <ul id="meals">
             {loadedMeals.map((meal)=> (
                 <li key={meal.id}>{meal.name}</li>
-            )
+           )
         )}
         </ul>
      )
