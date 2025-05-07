de1bb33929f2d306c6fe4ba2570907811a5e701c
Yasin <yasinpeker43@gmail.com>
2025-05-07 20:59:24 +0300

    header component css is done.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,13 @@
+import appLogo from '../assets/logo.jpg';
 export default function Header() {
     return (
-        <div id='header'>
-            
+        <div id='main-header'>
+            <div id="title">
+                <img src={appLogo} alt="a burger logo" />
+                <h1>REACTFOOD</h1>
+            </div>
+            <button className='button'>Cart</button>
+
         </div>
     )
 }
