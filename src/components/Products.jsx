c18b17826cfddbd0d85a70ef57b9a36ff62ac7fa
Yasin <yasinpeker43@gmail.com>
2025-05-07 21:31:04 +0300

    the data is fetched via an HTTP Request and with using useEffect for avoiding infinite loops.

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,8 +1,27 @@
+import { useEffect, useState } from "react";
+
 export default function Products() {
-  
-   const response = fetch('http:/localhost:3000/meals');
-     
-     return (
-        <ul id="meals"></ul>
+const [loadedMeals,setLoadedMeals]=useState([]);
+
+useEffect(()=> {
+    async function fetchingMeals() {
+       const  response =  await fetch('http:/localhost:3000/meals');
+    
+       if(!response.ok) {
+          // will be added later
+       }
+       const meals=await response.json();
+       setLoadedMeals(meals);
+    }
+    fetchingMeals();
+},[])
+
+  return (
+        <ul id="meals">
+            {loadedMeals.map((meal)=> (
+                <li key={meal.id}>{meal.name}</li>
+            )
+        )}
+        </ul>
      )
 }
