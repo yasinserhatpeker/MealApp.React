cfac322e4ed2296fff1bd19ab632bf2e76cd8a60
Yasin <yasinpeker43@gmail.com>
2025-05-10 15:33:09 +0300

    created UserProgressContext.

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,10 +1,28 @@
 import { useContext } from "react";
 import CartContext from "../store/CardContext";
+import Modal from "./Modal";
+import { currencyFormatter } from "../util/formatting";
 
 export default function Cart() {
-    
+
     cartCtx=useContext(CartContext);
     const cartTotal=cartCtx.items.reduce((totalPrice,item)=>
         totalPrice + item.price * item.quantity,0
+    )
+
+    return (
+        <Modal className='cart'>
+          <h2>Your Cart</h2>
+          <ul>
+            {cartCtx.items.map((item)=> 
+               <li key={item.id}>{item.name} - {item.quantity}</li>
+            )}
+          </ul>
+          <p className='cart-total'> {currencyFormatter.format(cartTotal)}</p>
+          <p>
+            <Button textOnly>Close</Button>
+            <Button>Go to Checkout Button</Button>
+          </p>
+        </Modal>
     )
 }
