f9bafa2effcac299ea908adad4628b078cb66f8b
Yasin <yasinpeker43@gmail.com>
2025-05-10 20:46:32 +0300

    a POST request is sent with Order data in Checkout component.

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -21,13 +21,27 @@ export default function Checkout() {
       const fd = new FormData(event.target);
       const customerData = Object.fromEntries(fd.entries());
 
+      fetch('http://localhost:3000/orders', {
+        method:'POST',
+         headers: {
+            'Content-type':'application/json'
+         },
+         body: JSON.stringify({
+            order: {
+                items:cartCtx.items,
+                customer:customerData,
+
+            }
+         })
+      })
+
   }
     return ( 
         <Modal>
             <form open={userProgressCtx.progress==='checkout'} onSubmit={handleSubmit}>
               <h2>Checkout</h2>
               <p>Total amount : {currencyFormatter.format(cartTotal)}</p>
-              <Input label="Full Name"  type="text" id="full-name"/>
+              <Input label="Full Name"  type="text" id="name"/>
               <Input label="Email Adress"  type="email" id="email"/>
               <Input label="Street"  type="text" id="street"/>
 
