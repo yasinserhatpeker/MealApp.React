352dca7dff39f00252b254432e385aa70aecaa99
Yasin <yasinpeker43@gmail.com>
2025-05-10 15:50:07 +0300

    userProgressCtx is added to the Header component for showing the cart.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import Header from "./components/Header";
 import Products from "./components/Products";
+import { UserProgressContextProvider } from "./components/UserProgressContext";
 import { CartContextProvider } from "./store/CardContext";
 
 function App() {
   return (
+    <UserProgressContextProvider>
     <CartContextProvider>
     <Header/>
     <Products/>
     </CartContextProvider>
+    </UserProgressContextProvider>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,19 @@ import { useContext } from 'react';
 import appLogo from '../assets/logo.jpg';
 import Button from '../UI/Button';
 import CartContext from '../store/CardContext';
+import UserProgressContext from './UserProgressContext';
 export default function Header() 
 {
    const cartCtx = useContext(CartContext);
+   const userProgressCtx=useContext(UserProgressContext);
 
    const totalCartItems = cartCtx.items.reduce((totalNumberOfItems,item) => {
      return totalNumberOfItems + item.quantity;
    },0) 
+
+   function handleShowCart() {
+     userProgressCtx.showCart();
+   }
     return (
         <header id='main-header'>
             <div id="title">
@@ -16,7 +22,7 @@ export default function Header()
                 <h1>PEKER'S</h1>
             </div>
             <nav>
-            <Button textOnly>Cart ({totalCartItems})</Button>
+            <Button textOnly onClick={handleShowCart}>Cart ({totalCartItems})</Button>
             </nav>
 
         </header>
